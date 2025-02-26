afe6df27f61f7db0aa910a6be07ac0f5900c97f9
l3xfrant3s <l3xfrant3s@outlook.com>
2025-02-26 11:07:13 +0000

    Ejercicio de colecciones

---
+++ Seccion 2 - Python basico/03-listas_tuplas_coleccion/12-ejercicio_colecciones.py
@@ -0,0 +1,17 @@
+# Create a list, called my_list, with three numbers. The total of the numbers added together should be 100.
+
+my_list = [25, 35, 40]
+
+print(sum(my_list))
+
+# Create a tuple, called my_tuple, with a single value in it
+
+my_tuple = (10,)  # Si no pongo la coma, Python lo interpreta como una variable sin más
+
+# Modify set2 so that set1.intersection(set2) returns {5, 77, 9, 12}
+set1 = {14, 5, 9, 31, 12, 77, 67, 8}
+set2 = {5, 77, 9, 12}
+
+set3 = set1.intersection(set2)
+
+print(set3)
