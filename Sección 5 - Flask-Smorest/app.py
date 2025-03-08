168b6d278fcca9ce7e8cbc068a461acc1c2be62f
l3xfrant3s <l3xfrant3s@outlook.com>
2025-03-08 18:25:39 +0000

    Empezamos a usar Flask-Smorest; manejo de errores básico

--- Sección 5 - Flask-Smorest/app.py
+++ Sección 5 - Flask-Smorest/app.py
@@ -1,6 +1,7 @@
 import uuid
 
 from flask import Flask, request
+from flask_smorest import abort
 
 from db import items, stores
 
@@ -10,6 +11,19 @@ app = Flask(__name__)
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+
+    # Comprobamos que la nueva tienda tiene campo name
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+
+    # Comprobamos que no exista una tienda con el mismo nombre
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="This store already exists")
+
     store_id = uuid.uuid4().hex  # Genera un nuevo id
     # Crea la tienda con los datos recibidos por JSON y el id
     store = {**store_data, "id": store_id}
@@ -20,8 +34,30 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+
+    # Comprobamos si el nuevo artículo tiene los campos price, store_id y name
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload.",
+        )
+
+    # Comprobamos si ya existe un artículo con el mismo nombre en la tienda que se vaya a crear
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="This item already exists")
+
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        # Cambiamos los return de errores 404 por la función de Flask/Flask-Smorest abort
+        abort(404, message="Store not found.")
+
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
@@ -38,12 +74,12 @@ def get_all_items():
     return {"items": list(items.values())}
 
 
-@app.get("/stores/<string:store_id>")
+@app.get("/store/<string:store_id>")
 def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:  # Saltará KeyError si no encuentra una clave en el diccionario
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
 
 @app.get("/item/<string:item_id>")
@@ -51,4 +87,4 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found.")
